a3ff76db90130f06e10a9ffc24a9c570f1428afa
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-12-21 08:16:11 +0100

    Add Koog project

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+rootProject.name = "koog-ssd"
+
+include(":prompt")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+        mavenLocal()
+    }
+}
